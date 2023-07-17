725cf4d4b1bdd6b53f05b67680d24e5d05b585c7
GeorginRMZ <87065916+GeorginRMZ@users.noreply.github.com>
2023-07-17 12:07:27 +0300

    Create stdint.h

---
+++ types/stdint.h
@@ -0,0 +1,10 @@
+typedef char int8_t;
+typedef unsigned char uint8_t;
+typedef uint8_t byte_t;
+
+typedef short int int16_t;
+typedef unsigned short int uint16_t;
+
+typedef long int int32_t;
+typedef unsigned long int uint32_t;
+typedef uint32_t size_t;
